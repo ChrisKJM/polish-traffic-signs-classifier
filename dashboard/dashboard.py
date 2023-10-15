5d1595557fac19c23f47d5967e4690669f2248e3
ChrisKJM <chriskjm0903@gmail.com>
2023-10-15 18:24:23 +0200

    Updated dashboard.py
    Changes:
    - added image rescaling to dashboard image preview
    - added sorting sign classes by their y and x coordinates in dashboard.py
    
    Fixes:
    - fixed a bug where some images uploaded to the dashboard would be rotated

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,27 +6,38 @@ import streamlit as st
 @st.cache_resource
 def append_project_folder():
     sys.path.append(os.path.abspath("."))
-    print(sys.path)
     return None
 
 append_project_folder()
 from detect_and_classify import detect_and_classify, CLASSES_FULL
-from PIL import Image, ImageDraw
-import json
+from PIL import Image, ImageDraw, ImageOps
 
 
 img = st.file_uploader(label="Select an image", type=["png", "jpg"])
 
 if img is not None:
     image = Image.open(img)
+    image = ImageOps.exif_transpose(image)
+
     result = detect_and_classify({"image": image})["image"]
+
+    scale = max(image.width, image.height) / 256
+    image = image.resize((int(image.width / scale), int(image.height // scale)))
+
+    # sort by y, then by x
+    result.sort(key=lambda val: (val["box"][1], val["box"][0]))
     draw = ImageDraw.Draw(image)
+
     text = ""
     i = 1
     for sign in result:
-        draw.rectangle(sign["box"], width=7, outline=(255, 0, 0))
+        draw.rectangle(tuple([sign["box"][i]/scale for i in range(4)]), width=5, outline=(255, 0, 0))
         text += f"{i}. {CLASSES_FULL[sign['prediction']]}\n"
         i += 1
+
+    if len(result) == 0:
+        text = "No signs detected"
+
     st.image(image)
     st.text(text)
     image.close()
